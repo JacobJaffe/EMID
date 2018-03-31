15a30ba349c041f398266476a5d17fc37e070ea6
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-03-31 13:15:40 -0400

    camera

--- src/detect_markers.py
+++ src/detect_markers.py
@@ -8,11 +8,18 @@ Marker_Top_Right = []
 Marker_Bot_Left = []
 Marker_Bot_Right = []
 
-# shitty code to ensure all have been found
+# so we don't warp untill we find corners
+cornersFound = False
+
+# shitty (shidi) code to ensure all have been found
 a = False
 b = False
 c = False
 d = False
+
+# global state of last points to use for projection
+corner_centers = np.array([[-1,-1], [-1,-1], [-1,-1], [-1,-1]])
+
 
 def getCornerCenter(corner):
     x = (corner[0][0] + corner[1][0] + corner[2][0] + corner[3][0]) / 4
@@ -28,7 +35,7 @@ if __name__ == "__main__":
         # Capture frame-by-frame
         ret, frame = cap.read()
         h, w = frame.shape[:2]
-        frame_blank = np.zeros((h, w,3), np.uint8)
+        frame_projection = np.zeros((h, w,3), np.uint8)
 
         # Our operations on the frame come here
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -56,10 +63,10 @@ if __name__ == "__main__":
                     Marker_Bot_Right = corners[i][0]
                     d = True
 
+        # recompute the corners if possible
         if (a and b and c and d):
-
+            cornersFound = True
             corners = [Marker_Top_Left, Marker_Bot_Left, Marker_Top_Right, Marker_Bot_Right]
-            corner_centers = np.array([[-1,-1], [-1,-1], [-1,-1], [-1,-1]])
 
             index = 0
             for corner in corners:
@@ -74,16 +81,19 @@ if __name__ == "__main__":
                 # iterate
                 index = index + 1
 
-            # warp the image based on these corners:
+        # If u want them in the same window
+        # frame_side_by_side = np.hstack((frame, frame_projection))
+
+        if (cornersFound):
+            # warp the image based on last found corners:
             warp = four_point_transform(frame, corner_centers)
 
             # paste warp in the blank frame
-            frame_blank[0:0+warp.shape[0], 0:0+warp.shape[1]] = warp
-
-        frame = np.hstack((frame, frame_blank))
+            frame_projection[0:0+warp.shape[0], 0:0+warp.shape[1]] = warp
 
         # Display the resulting frame
-        cv2.imshow('frame',frame)
+        cv2.imshow('Camera',frame)
+        cv2.imshow('Projection', frame_projection)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
