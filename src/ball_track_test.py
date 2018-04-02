132528403e8ea1e7512f8264661eae7b2210d1fc
Dan Meyer <cpudan@gmail.com>
2018-04-02 15:10:39 -0400

    Red ball detected

--- src/ball_track_test.py
+++ src/ball_track_test.py
@@ -16,17 +16,15 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+greenLower = (0, 126, 53)
+greenUpper = (4, 255, 255)
 
-redLower = (200, 0, 0)
-redUpper = (255, 130, 130)
 pts = deque(maxlen=args["buffer"])
 
 # if a video path was not supplied, grab the reference
 # to the webcam
 if not args.get("video", False):
-	camera = cv2.VideoCapture(1)
+	camera = cv2.VideoCapture(0)
 
 # otherwise, grab a reference to the video file
 else:
@@ -54,6 +52,27 @@ while True:
 	mask = cv2.inRange(hsv, greenLower, greenUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
+	# find contours in the mask and initialize the current
+	# (x, y) center of the ball
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)[-2]
+	center = None
+
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+		# draw the circle and centroid on the frame,
+		# then update the list of tracked points
+		cv2.circle(frame, (int(x), int(y)), int(radius),
+			(0, 255, 255), 2)
+		cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
 
 	# loop over the set of tracked points
 	for i in range(1, len(pts)):
