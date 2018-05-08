6c8a2477d74e34fcc252d10032506d11d41326e4
Jacob Jaffe <jacob.jaffe@tufts.edu>
2018-05-08 17:02:08 -0400

    double range fix

--- range-detector.py
+++ range-detector.py
@@ -11,19 +11,24 @@ import cv2
 import argparse
 from operator import xor
 
+WINDOW_1 = "Window 1"
+WINDOW_2 = "Window 2"
 
 def callback(value):
     pass
 
 
-def setup_trackbars(range_filter):
-    cv2.namedWindow("Trackbars", 0)
+def setup_trackbars(range_filter, window):
+    cv2.namedWindow(window, 0)
 
     for i in ["MIN", "MAX"]:
         v = 0 if i == "MIN" else 255
 
         for j in range_filter:
-            cv2.createTrackbar("%s_%s" % (j, i), "Trackbars", v, 255, callback)
+            cv2.createTrackbar("%s_%s" % (j, i), window, v, 255, callback)
+
+    cv2.createTrackbar("ERODE_ITERATIONS", window, 0, 4, callback)
+    cv2.createTrackbar("DILATE_ITERATIONS", window, 0, 4, callback)
 
 
 def get_arguments():
@@ -48,15 +53,27 @@ def get_arguments():
     return args
 
 
-def get_trackbar_values(range_filter):
+def get_trackbar_values(range_filter, window):
     values = []
 
     for i in ["MIN", "MAX"]:
         for j in range_filter:
-            v = cv2.getTrackbarPos("%s_%s" % (j, i), "Trackbars")
+            v = cv2.getTrackbarPos("%s_%s" % (j, i), window)
             values.append(v)
 
+    _ERODE_ITERATIONS = cv2.getTrackbarPos("ERODE_ITERATIONS", window)
+    _DILATE_ITERATIONS  = cv2.getTrackbarPos("DILATE_ITERATIONS", window)
+    values.append(_ERODE_ITERATIONS)
+    values.append(_DILATE_ITERATIONS)
+
     return values
+
+def getThresh(range_filter, window, frame_to_thresh):
+    v1_min, v2_min, v3_min, v1_max, v2_max, v3_max, ERODE_ITERATIONS, DILATE_ITERATIONS = get_trackbar_values(range_filter, window)
+    thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+    thresh = cv2.erode(thresh, None, iterations=ERODE_ITERATIONS)
+    thresh = cv2.dilate(thresh, None, iterations=DILATE_ITERATIONS)
+    return thresh
 
 
 def main():
@@ -72,9 +89,10 @@ def main():
         else:
             frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     else:
-        camera = cv2.VideoCapture(0)
+        camera = cv2.VideoCapture(1)
 
-    setup_trackbars(range_filter)
+    setup_trackbars(range_filter, WINDOW_1)
+    setup_trackbars(range_filter, WINDOW_2)
 
     while True:
         if args['webcam']:
@@ -88,15 +106,17 @@ def main():
             else:
                 frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
+        thresh_1 = getThresh(range_filter, WINDOW_1, frame_to_thresh)
+        thresh_2 = getThresh(range_filter, WINDOW_2, frame_to_thresh)
 
-        thresh = cv2.inRange(frame_to_thresh, (v1_min, v2_min, v3_min), (v1_max, v2_max, v3_max))
+        thresh = thresh_1 | thresh_2
+
 
         if args['preview']:
             preview = cv2.bitwise_and(image, image, mask=thresh)
             cv2.imshow("Preview", preview)
         else:
-            cv2.imshow("Original", image)
+            #cv2.imshow("Original", image)
             cv2.imshow("Thresh", thresh)
 
         if cv2.waitKey(1) & 0xFF is ord('q'):
